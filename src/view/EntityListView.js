3296d349a4afbd6f8e3a2f86b0b73f212ac7b520
Anton De Silva <gardhiwasam@yahoo.com>
2020-10-18 16:27:48 -0400

    Update EntityListView.js

--- src/view/EntityListView.js
+++ src/view/EntityListView.js
@@ -9,15 +9,19 @@
 */
 export const renderEntityList = (columnConfig, data) => {
     let tableHeaders = `<tr>${columnConfig.map(column => `<th>${column.title}</th>`).join('\n')}</tr>`;
-    let tableRows = data.map(dataObj => generateGridRow(dataObj, columnConfig)).join('\n')
+    let tableRows = data.map(dataObj => generateGridRow(columnConfig, dataObj)).join('\n')
     return `
-        <table>
-            ${tableHeaders}
-            ${tableRows}
-        </table>
+        <div class="data-grid">
+            <table>
+                <tbody>
+                    ${tableHeaders}
+                    ${tableRows}
+                </tbody>
+            </table>
+        </div>
     `
 }
 
-generateGridRow = (columnConfig, dataObj) => {
+const generateGridRow = (columnConfig, dataObj) => {
     return `<tr>${columnConfig.map(column => `<td>${dataObj[column.dataKey]}</td>`).join('\n')}</tr>`;
 }
